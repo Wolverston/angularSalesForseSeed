2faf94220f1755d088ed291bb6d707ba09c71e88
Sorochan Roman <sorochan.roman@gmail.com>
2017-03-09 14:27:18 +0200

    fix router bug

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -2,13 +2,41 @@
   'use strict';
 
   angular
-    .module('template')
-    .config(function($routeProvider) {
-      $routeProvider
-      .when('/', {
-        templateUrl: '../app/main/main.html',
-        controller: 'mainCtrl',
-        controllerAs: 'mainCtrl'
-      })
-    });
+  .module('template')
+   .config(function($routeProvider) {
+    $routeProvider
+    .when('/dashboard', {
+       templateUrl: '../app/main/main.html',
+      controller: 'mainCtrl',
+      controllerAs: 'mainCtrl'
+    })
+  })
 })();
+
+
+
+// (function() {
+//   'use strict';
+
+//   angular
+//   .module('inspinia')
+
+//   /** @ngInject */
+//   .config(function($routeProvider) {
+//     $routeProvider
+//     .when('/dashboard', {
+//       templateUrl: 'app/home/VP_HomePage.html',
+//       controller: 'homeCtrl',
+//       controllerAs: 'homeCtrl'
+//     })
+//     .otherwise({redirectTo: '/dashboard'})
+//   })
+// })();
+
+
+
+// .when('/', {
+//       templateUrl: '../app/main/main.html',
+//       controller: 'mainCtrl',
+//       controllerAs: 'mainCtrl'
+//     })
